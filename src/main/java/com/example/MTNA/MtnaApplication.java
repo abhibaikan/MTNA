623bf711e3016548fa6ecb8d6081ecff2faf5a3a
Abhijeet <Abhibaikan@gmail.com>
2019-05-02 11:52:58 +0530

    initial commit

---
+++ src/main/java/com/example/MTNA/MtnaApplication.java
@@ -0,0 +1,13 @@
+package com.example.MTNA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtnaApplication.class, args);
+	}
+
+}
